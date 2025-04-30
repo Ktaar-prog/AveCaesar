ce5b6ee8afdba8c082cfd642c164bc71f0e163d3
Volodymyr Marchenko <vladimir.aur7@gmail.com>
2025-04-30 05:51:47 +0300

    Added method shiftText in CaesarCipher

--- src/main/java/com/ktaar/encryption/CaesarCipher.java
+++ src/main/java/com/ktaar/encryption/CaesarCipher.java
@@ -1,11 +1,17 @@
 package com.ktaar.encryption;
 
+import static com.ktaar.constants.Constants.CHARACTERS_EN;
+
 public class CaesarCipher {
 
     public String shiftText(String text, int shift) {
         StringBuilder result = new StringBuilder();
         for (char c : text.toCharArray()) {
-//            code
+            if (CHARACTERS_EN.contains(c)) {
+                char cTemp = CHARACTERS_EN.get(CHARACTERS_EN.indexOf(c) + shift);
+                result.append(cTemp);
+            }
+            result.append(c);
         }
 
         return result.toString();
